46a73629905e77c82ca14f4bbbd0606917acb564
Neceros <neceros@gmail.com>
2020-03-26 21:17:23 -0700

    Initial commit

---
+++ Source/MoreSteel/ModSettings.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using UnityEngine;
+using Verse;
+
+namespace MoreSteel
+{
+  public class MoreSteelModSettings : ModSettings
+  {
+    public static float multiplyMineableSteel => multiplyMS;
+    public static int OriginalSteelAmount;
+
+    // Internal reference only. DO NOT call outside of this class.
+    public static float multiplyMS = 1.5f;
+    // End warning
+
+    public override void ExposeData()
+    {
+      base.ExposeData();
+      Scribe_Values.Look(ref multiplyMS, "MSAmountToMultiplySteel");
+    }
+  }
+
+  public class MoreSteelMod : Mod
+  {
+    MoreSteelModSettings settings;
+    public MoreSteelMod(ModContentPack con) : base(con)
+    {
+      this.settings = GetSettings<MoreSteelModSettings>();
+    }
+
+    public override void DoSettingsWindowContents(Rect inRect)
+    {
+      Listing_Standard listing = new Listing_Standard();
+      listing.Begin(inRect);
+
+      if (MoreSteelModSettings.OriginalSteelAmount >= 1)
+      {
+        listing.Label("MSRestartWarning".Translate());
+        listing.Gap(36);
+        listing.Label("MSMultiplyAmountLabel".Translate() + ": [" + MoreSteelModSettings.OriginalSteelAmount.ToString() + " x " + (MoreSteelModSettings.multiplyMS * 100).ToString() + "%] = " + (MoreSteelModSettings.OriginalSteelAmount * MoreSteelModSettings.multiplyMS).ToString() + " " + "MSMultiplyAmountEndLabel".Translate());
+        MoreSteelModSettings.multiplyMS = RoundToNearestHalf(listing.Slider(MoreSteelModSettings.multiplyMS, 0f, 20f));
+        if (MoreSteelModSettings.OriginalSteelAmount * MoreSteelModSettings.multiplyMS >= 75)
+          listing.Label("MSStackWarning".Translate());
+      }
+      else
+      {
+        listing.Label("MSOnlyInGame".Translate());
+      }
+
+      listing.End();
+      base.DoSettingsWindowContents(inRect);
+    }
+
+    public override string SettingsCategory()
+    {
+      return "MSTitle".Translate();
+    }
+
+    private float RoundToNearestHalf(float val)
+    { 
+      return (float)Math.Round(val * 2, MidpointRounding.AwayFromZero) / 2;
+    }
+  }
+}
---
+++ Source/MoreSteel/WorldComponent.cs
@@ -0,0 +1,26 @@
+﻿using RimWorld.Planet;
+using System;
+using Verse;
+
+namespace MoreSteel
+{
+  internal class MoreSteelWorldComp : WorldComponent
+  {
+    public MoreSteelWorldComp(World world)
+      : base(world)
+    {}
+
+    public override void FinalizeInit()
+    {
+      base.FinalizeInit();
+
+      MoreSteelModSettings.OriginalSteelAmount = DefDatabase<ThingDef>.GetNamed("MineableSteel").building.mineableYield;
+      UpdateAllChanges();
+    }
+
+    public void UpdateAllChanges()
+    {
+      DefDatabase<ThingDef>.GetNamed("MineableSteel").building.mineableYield = (int)Math.Floor(MoreSteelModSettings.OriginalSteelAmount * MoreSteelModSettings.multiplyMS);
+    }
+  }
+}
